ef75fb155ff66e42a28f6499d851e8864903990d
tron <kishs1991@gmail.com>
2016-07-20 12:01:25 +0530

    changes reflected in database for user, group and thought text tables

--- mythoughts/mythoughts/settings.py
+++ mythoughts/mythoughts/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'me.apps.MeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
