77eed378086217c578f7b8d506319514dc285013
tron <kishs1991@gmail.com>
2016-07-20 11:50:47 +0530

    created basic models for user, group and thought text

--- mythoughts/me/models.py
+++ mythoughts/me/models.py
@@ -3,3 +3,24 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class UserTable(models.Model):
+    username = models.CharField(max_length=250)
+    password = models.CharField(max_length=250)
+    status = models.CharField(max_length=100)
+    group = models.CharField(max_length=250)
+
+
+class GroupTable(models.Model):
+    groupName = models.CharField(max_length=250)
+    status = models.CharField(max_length=100)
+    numberOfMembers = models.IntegerField()
+
+
+class ThoughtTable(models.Model):
+    username = models.CharField(max_length=250)
+    thoughtText = models.TextField()
+    lastUpdateTime = models.DateTimeField()
+    md5HashOfText = models.TextField()
+    hashTag = models.TextField()
